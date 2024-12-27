62c5fa7418e1b77ec1bdc628b1d2b850d4994610
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-27 01:08:31 -0500

    Add metadata to root layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Home",
+};
+
+const HomePage = () => {
+  return (
+    <>
+      <Button>Button</Button>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "VistaShopVerse";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Best online shopping experience";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
