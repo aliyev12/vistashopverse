ad7044b60ce76bd4c618646563b5636be85197d5
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-27 20:34:52 -0500

    Add metadata to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "VistaShopVerse",
-  description: "Best online shopping experience",
+  title: {
+    template: `%s | VistaShopVerse`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
