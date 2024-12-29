06965d92b4099fc13ab5a17fe057db49a5e37c16
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-28 23:56:52 -0500

    Test newly dockarized next app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+// import pg from "pg";
 
 export const metadata = {
   title: "Home",
 };
 
-const HomePage = () => {
+const HomePage = async () => {
+  // // TODO : Function to test database connection, remove when not needed
+  // (async function testDatabase() {
+  //   const pool = new pg.Pool({
+  //     connectionString: process.env.VSV_DATABASE_URL,
+  //   });
+  //   const client = await pool.connect();
+  //   const result = await client.query("SELECT * FROM test");
+  //   client.release();
+  //   console.log("Database test result = ", result.rows);
+  // })();
+
   return (
     <>
       <ProductList
