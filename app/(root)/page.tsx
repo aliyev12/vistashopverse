1558daa1c972edef2af522c90f84020913e43f18
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-29 22:07:01 -0500

    Clean up left over docker code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,33 +1,17 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
-import pg from "pg";
 
 export const metadata = {
   title: "Home",
 };
 
 const HomePage = async () => {
-  // TODO : Function to test database connection, remove when not needed
-  async function testDatabase() {
-    const pool = new pg.Pool({
-      connectionString: process.env.VSV_DATABASE_URL,
-    });
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM "Product"');
-    client.release();
-    console.log("Database test result = ", result.rows);
-    return result.rows;
-  }
-
-  const results = await testDatabase();
-
   const latestProducts = await getLatestProducts();
 
   return (
-    <div>
-      <div>{JSON.stringify(results)}</div>
+    <>
       <ProductList data={latestProducts} title="Newest Arrivals" />
-    </div>
+    </>
   );
 };
 
