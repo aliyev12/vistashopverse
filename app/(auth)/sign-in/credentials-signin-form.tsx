b285cdb9bcf21c61c4d922e7ea40d53e7b4ea7bd
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-31 11:41:39 -0500

    Build credentials fign in form inputs

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import Link from "next/link";
 
 const CredentialsSignInForm = () => {
   return (
@@ -15,7 +18,30 @@ const CredentialsSignInForm = () => {
             type="email"
             required
             autoComplete="email"
+            defaultValue={signInDefaultValues.email}
           />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <Button className="w-full" variant="default">
+            Sign in
+          </Button>
+        </div>
+        <div className="text-sm text-center text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign up
+          </Link>
         </div>
       </div>
     </form>
