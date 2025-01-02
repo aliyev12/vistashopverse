55031cef78686c746efb3ea25acecd2c80ede3fc
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-01 20:45:16 -0500

    Create add to cart component and server action and add them to product page

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { CartItem } from "@/types";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  async function handleAddToCart() {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast({
+        variant: "destructive",
+        description: res.message,
+      });
+
+      return;
+    }
+
+    // Handle success add to cart
+    toast({
+      description: `${item.name} added to cart`,
+      action: (
+        <ToastAction
+          className="bg-primary text-white hover:bg-gray-800"
+          altText="Go to cart"
+          onClick={() => router.push("/cart")}
+        >
+          Go to cart
+        </ToastAction>
+      ),
+    });
+  }
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus /> Add to cart
+    </Button>
+  );
+};
+
+export default AddToCart;
