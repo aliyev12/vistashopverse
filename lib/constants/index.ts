1e364f1bf7ef917076b601f50b69689b73c466a9
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-02 20:59:42 -0500

    Handle adding to cart and persisting to db

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -5,6 +5,19 @@ export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.VSV_LATEST_PRODUCTS_LIMIT) || 4;
+export const TAX = Number(process.env.NEXT_TAX) || 0.15;
+
+/**
+ * Minimum purchase amount to get free shipping.
+ * @default 100
+ */
+export const SHIPPING_MIN = Number(process.env.NEXT_SHIPPING_MIN) || 100;
+
+/**
+ * Default shipping cost for purchases less than $100
+ * @default 10
+ */
+export const SHIPPING_DEFAULT = Number(process.env.NEXT_SHIPPING_DEFAULT) || 10;
 
 export const signInDefaultValues = {
   email: "admin@example.com",
