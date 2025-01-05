b0cb352e7266836ab9b9ee574800d79b4fa016c2
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-05 00:47:25 -0500

    Create an order page

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -4,7 +4,7 @@ import { auth } from "@/auth";
 import { prisma } from "@/db/prisma";
 import { CartItem } from "@/types";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
-import { formatErrors } from "../utils";
+import { convertToPlainObject, formatErrors } from "../utils";
 import { insertOrderSchema } from "../validators";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
@@ -104,4 +104,19 @@ export async function createOrder() {
       message: formatErrors(error),
     };
   }
+}
+
+// Get order by id
+export async function getOrderById(orderId: string) {
+  const data = await prisma.order.findFirst({
+    where: { id: orderId },
+    include: {
+      orderitems: true,
+      user: {
+        select: { name: true, email: true },
+      },
+    },
+  });
+
+  return convertToPlainObject(data);
 }
