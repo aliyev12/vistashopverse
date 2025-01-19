6070958af3a09e09dc560f72e60db61fabf27c59
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-18 22:05:44 -0500

    Add unit test to test JWT access token tom paypal

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -4,10 +4,10 @@ export const paypal = {};
 
 // Generate paypal access token
 async function generateAccessToken() {
-  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
-  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
-    "base64"
-  );
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET_KEY } = process.env;
+  const auth = Buffer.from(
+    `${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET_KEY}`
+  ).toString("base64");
   const response = await fetch(`${base}/v1/oauth2/token`, {
     method: "POST",
     body: "grant_type=client_credentials",
@@ -25,3 +25,5 @@ async function generateAccessToken() {
     throw new Error(errorMessage);
   }
 }
+
+export { generateAccessToken };
