78657d99e8971e727456db4859904d5c2dc17fe9
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-26 01:29:32 -0500

    Add search functinality to admin products page

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -36,7 +36,19 @@ const AdminProductsPage = async (props: {
   return (
     <div className="space-y-2">
       <div className="flex-between">
-        <h1 className="h2-bold">Products</h1>
+        <div className="flex flex-col items-start gap-3">
+          <h1 className="h2-bold">Products</h1>
+          {searchText && (
+            <div>
+              Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+              <Link href="/admin/products" className="ml-3">
+                <Button variant="outline" size="sm">
+                  Remove filter
+                </Button>
+              </Link>
+            </div>
+          )}
+        </div>
         <Button asChild variant="default">
           <Link href="/admin/products/create">Create product</Link>
         </Button>
