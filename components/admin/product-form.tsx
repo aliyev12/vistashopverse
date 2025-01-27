ba507f47ee99eebc4003db669c51eed320dbf1dd
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-26 21:41:19 -0500

    Add image download input field from Uploadthing component

--- components/admin/product-form.tsx
+++ components/admin/product-form.tsx
@@ -21,6 +21,9 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { createProduct, updateProduct } from "@/lib/actions/product.actions";
+import { UploadButton } from "@/lib/uploadthing";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
 
 const ProductForm = ({
   type,
@@ -91,6 +94,8 @@ const ProductForm = ({
       router.push(`/admin/products`);
     }
   };
+
+  const images = form.watch("images");
 
   type TNameField = {
     field: ControllerRenderProps<z.infer<typeof insertProductSchema>, "name">;
@@ -226,7 +231,49 @@ const ProductForm = ({
             )}
           />
         </div>
-        <div className="upload-field flex flex-col gap-5">{/* Images */}</div>
+        <div className="upload-field flex flex-col gap-5">
+          {/* Images */}
+          <FormField
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel>Images</FormLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="Product image"
+                          className="w-20 h-20 object-cover object-center rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue("images", [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast({
+                              variant: "destructive",
+                              description: `Error from Uploadthing: ${error.message}`,
+                            });
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
         <div className="upload-field">{/* isFeatured */}</div>
         <div>
           {/* Description */}
