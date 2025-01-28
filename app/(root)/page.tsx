698971628df2403e3a5c7c5138ceac2d657558e6
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-27 19:37:28 -0500

    Add carousel on home page for all of the features products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 
 export const metadata = {
   title: "Home",
@@ -7,9 +11,13 @@ export const metadata = {
 
 const HomePage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
