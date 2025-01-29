901f4ad79d2788cd95577feca8e2055d96865106
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-28 23:19:36 -0500

    Remove comments from auth.ts trying to reduce file size

--- auth copy.ts
+++ auth copy.ts
@@ -26,16 +26,22 @@ export const config = {
       // @ts-ignore
       async authorize(credentials) {
         if (credentials === null) return null;
+
+        // Find user in database
         const user = await prisma.user.findFirst({
           where: {
             email: credentials.email as string,
           },
         });
+
+        // Check if user exists and if the password matches
         if (user && user.password) {
           const isMatch = compareSync(
             credentials.password as string,
             user.password
           );
+
+          // If password is correct, return user
           if (isMatch) {
             return {
               id: user.id,
@@ -44,6 +50,8 @@ export const config = {
               role: user.role,
             };
           }
+
+          // If user does not exist or password does not match return null
           return null;
         }
       },
@@ -52,20 +60,29 @@ export const config = {
 
   callbacks: {
     async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
+
+      // If there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
+
       return session;
     },
     async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
       if (user) {
         token.id = user.id;
         token.role = user.role;
+
+        // If user has no name then use the email
         if (user.name === "NO_NAME") {
           token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
@@ -80,10 +97,14 @@ export const config = {
             const sessionCart = await prisma.cart.findFirst({
               where: { sessionCartId },
             });
+
             if (sessionCart) {
+              // Delete current user cart
               await prisma.cart.deleteMany({
                 where: { userId: user.id },
               });
+
+              // Assign new cart
               await prisma.cart.update({
                 where: { id: sessionCart.id },
                 data: { userId: user.id },
@@ -92,6 +113,8 @@ export const config = {
           }
         }
       }
+
+      // Handle session updates
       if (session?.user.name && trigger === "update") {
         token.name = session.user.name;
       }
@@ -99,6 +122,7 @@ export const config = {
       return token;
     },
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
       const protectedPaths = [
         /\/shipping-address/,
         /\/payment-method/,
@@ -108,17 +132,31 @@ export const config = {
         /\/order\/(.*)/,
         /\/admin/,
       ];
+
+      // Get pathname from the request URL object
       const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
       if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       if (!request.cookies.get("sessionCartId")) {
+        // Check for session cart cookie
+        // Generate new session cart id cookie
         const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
         const newRequestHeaders = new Headers(request.headers);
+
+        // Create new response and add new headers
         const response = NextResponse.next({
           request: {
             headers: newRequestHeaders,
           },
         });
+
+        // Set newly generated sessionCartId in the response cookies
         response.cookies.set("sessionCartId", sessionCartId);
+
         return response;
       } else {
         return true;
