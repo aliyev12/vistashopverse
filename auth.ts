be67469fdf069e0be4fb1985de25286947b77e1f
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-28 22:28:23 -0500

    Minimize next auth middleware to reduce the size of edge function

--- auth.ts
+++ auth.ts
@@ -3,9 +3,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
-import type { NextAuthConfig } from "next-auth";
+import { authConfig } from "./auth.config";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -13,7 +12,7 @@ export const config = {
     error: "/sign-in",
   },
   session: {
-    strategy: "jwt",
+    strategy: "jwt" as const,
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
   adapter: PrismaAdapter(prisma),
@@ -26,16 +25,22 @@ export const config = {
       // @ts-ignore
       async authorize(credentials) {
         if (credentials === null) return null;
+
+        // Find user in database
         const user = await prisma.user.findFirst({
           where: {
             email: credentials.email as string,
           },
         });
+
+        // Check if user exists and if the password matches
         if (user && user.password) {
           const isMatch = compareSync(
             credentials.password as string,
             user.password
           );
+
+          // If password is correct, return user
           if (isMatch) {
             return {
               id: user.id,
@@ -44,6 +49,8 @@ export const config = {
               role: user.role,
             };
           }
+
+          // If user does not exist or password does not match return null
           return null;
         }
       },
@@ -51,21 +58,31 @@ export const config = {
   ],
 
   callbacks: {
+    ...authConfig.callbacks,
     async session({ session, user, trigger, token }: any) {
+      // Set the user ID from the token
       session.user.id = token.sub;
       session.user.role = token.role;
       session.user.name = token.name;
+
+      // If there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
+
       return session;
     },
     async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
       if (user) {
         token.id = user.id;
         token.role = user.role;
+
+        // If user has no name then use the email
         if (user.name === "NO_NAME") {
           token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
           await prisma.user.update({
             where: { id: user.id },
             data: { name: token.name },
@@ -80,10 +97,14 @@ export const config = {
             const sessionCart = await prisma.cart.findFirst({
               where: { sessionCartId },
             });
+
             if (sessionCart) {
+              // Delete current user cart
               await prisma.cart.deleteMany({
                 where: { userId: user.id },
               });
+
+              // Assign new cart
               await prisma.cart.update({
                 where: { id: sessionCart.id },
                 data: { userId: user.id },
@@ -92,39 +113,15 @@ export const config = {
           }
         }
       }
+
+      // Handle session updates
       if (session?.user.name && trigger === "update") {
         token.name = session.user.name;
       }
 
       return token;
     },
-    authorized({ request, auth }: any) {
-      const protectedPaths = [
-        /\/shipping-address/,
-        /\/payment-method/,
-        /\/place-order/,
-        /\/profile/,
-        /\/user\/(.*)/,
-        /\/order\/(.*)/,
-        /\/admin/,
-      ];
-      const { pathname } = request.nextUrl;
-      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
-      if (!request.cookies.get("sessionCartId")) {
-        const sessionCartId = crypto.randomUUID();
-        const newRequestHeaders = new Headers(request.headers);
-        const response = NextResponse.next({
-          request: {
-            headers: newRequestHeaders,
-          },
-        });
-        response.cookies.set("sessionCartId", sessionCartId);
-        return response;
-      } else {
-        return true;
-      }
-    },
   },
-} satisfies NextAuthConfig;
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
