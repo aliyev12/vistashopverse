ce26943d1b7afaad6883f17429f52353e4995b37
Abdul Aliyev <aaa7c4@gmail.com>
2025-01-30 18:27:34 -0500

    Add stripe payment option to order details table

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -30,15 +30,18 @@ import {
   updateOrderToPaidCOD,
   deliverOrder,
 } from "@/lib/actions/order.actions";
+import StripePayment from "./stripe-payment";
 
 const OrderDetailsTable = ({
   order,
   paypalClientId,
   isAdmin,
+  stripeClientSecret,
 }: {
   order: Order;
   paypalClientId: string;
   isAdmin: boolean;
+  stripeClientSecret: string | null;
 }) => {
   const {
     shippingAddress,
@@ -205,6 +208,15 @@ const OrderDetailsTable = ({
                 />
               </PayPalScriptProvider>
             </div>
+          )}
+
+          {/* Stripe Payment */}
+          {!isPaid && paymentMethod === "Stripe" && stripeClientSecret && (
+            <StripePayment
+              priceInCents={Number(order.totalPrice) * 100}
+              orderId={order.id}
+              clientSecret={stripeClientSecret}
+            />
           )}
 
           {/* Cash on Delivery (COD) */}
